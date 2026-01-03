a290c0ab724364d9a7d3229e39a72f93b663f6bc
zaheenrakib <rakibzaheen@gmail.com>
2026-01-03 14:14:01 +0600

    Summery

--- summery.js
+++ summery.js
@@ -23,3 +23,27 @@
 // const numbers = [1, 2, 3, 4, 5, 55];
 // console.log(...numbers);
 
+// const person = {
+//   name: "Rakib",
+//   age: 24
+// }
+
+// const { name, age } = person;
+// console.log(name, age);
+
+// const frnd = ["Hero Alom", "Mala"];
+
+// const [first, second] = frnd;
+// console.log(first, second);
+
+const person = {
+  name: "Rakib",
+  age: 24
+}
+
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
+
+Object.seal(person);
+Object.freeze(person);
