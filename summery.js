f92daa3ce473ca822fa0abe6b9f444df126cc030
zaheenrakib <rakibzaheen@gmail.com>
2026-01-03 14:09:07 +0600

    summery

---
+++ summery.js
@@ -0,0 +1,25 @@
+// var not use
+
+// const and let
+// const countryName = "BD"
+
+// let age = 10;
+// age = 20;
+
+// console.log(`Loidjaj
+//   dkak
+//   kakf`);
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+  
+// const sum2 = (num1, num2) => num1 + num2;
+
+// const sum3 = (num1, num2) => {
+//   return num1 + num2;
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 55];
+// console.log(...numbers);
+
